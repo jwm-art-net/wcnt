71b36d2d72678632cfd4f888629a759a3bbc214f
James Morris <james@jwm-art.net>
2013-09-18 22:29:24 +0100

    extremely minor fxsparamlist fix

--- src/fxsparamlist.cc
+++ src/fxsparamlist.cc
@@ -5,9 +5,11 @@ fxsparamlist::add_param(char const* str_list, paramnames::PAR_TYPE pt)
 {
     if (!str_list)
         return 0;
-    if (get_fix_str_param(pt))
-        return 0; // already added
-    fixstrparam* fsp = new fixstrparam(str_list, pt);
+
+    fixstrparam* fsp = 0;
+    if ((fsp = get_fix_str_param(pt)))
+        return fsp; // already added
+    fsp = new fixstrparam(str_list, pt);
     if (!add_at_tail(fsp)) {
         delete fsp;
         return 0;
